df3f1751fa4187ef4c1162ed785399fcc508c981
pragtech <admin@pragtech.co.in>
2019-05-02 19:33:26 +0530

    update module pragtech_crm_activity and pragtech_email_compose

--- pragtech_crm_activity/models/mail_activity.py
+++ pragtech_crm_activity/models/mail_activity.py
@@ -1,5 +1,5 @@
 from odoo import fields, api, models
-
+from collections import defaultdict
 
 class MailActivity(models.Model):
     _inherit = 'mail.activity'
@@ -36,6 +36,57 @@ class MailActivity(models.Model):
                 crm_lead_id.update({'next_activity_deadline': self.datetime_deadline})
         return res
     
+    
+    @api.model
+    def get_activity_data(self, res_model, domain):
+        res = self.env[res_model].search(domain)
+        activity_domain = [('res_id', 'in', res.ids), ('res_model', '=', res_model)]
+        grouped_activities = self.env['mail.activity'].read_group(
+            activity_domain,
+            ['res_id', 'activity_type_id', 'res_name:max(res_name)', 'ids:array_agg(id)', 'datetime_deadline:min(datetime_deadline)'],
+            ['res_id', 'activity_type_id'],
+            lazy=False)
+        activity_type_ids = self.env['mail.activity.type']
+        res_id_to_name = {}
+        res_id_to_deadline = {}
+        activity_data = defaultdict(dict)
+        for group in grouped_activities:
+            res_id = group['res_id']
+            res_name = group['res_name']
+            activity_type_id = group['activity_type_id'][0]
+            activity_type_ids |= self.env['mail.activity.type'].browse(activity_type_id)  # we will get the name when reading mail_template_ids
+            res_id_to_name[res_id] = res_name
+            res_id_to_deadline[res_id] = group['datetime_deadline'] if (res_id not in res_id_to_deadline or group['datetime_deadline'] < res_id_to_deadline[res_id]) else res_id_to_deadline[res_id]
+            state = self._compute_state_from_date(group['datetime_deadline'], self.user_id.sudo().tz)
+            activity_data[res_id][activity_type_id] = {
+                'count': group['__count'],
+                'domain': group['__domain'],
+                'ids': group['ids'],
+                'state': state,
+                'o_closest_deadline': group['datetime_deadline'],
+            }
+        res_ids_sorted = sorted(res_id_to_deadline, key=lambda item: res_id_to_deadline[item])
+        activity_type_infos = []
+        for elem in sorted(activity_type_ids, key=lambda item: item.sequence):
+            mail_template_info = []
+            for mail_template_id in elem.mail_template_ids:
+                mail_template_info.append({"id": mail_template_id.id, "name": mail_template_id.name})
+            activity_type_infos.append([elem.id, elem.name, mail_template_info])
+        res_id_lst = []
+        #Adding Partner Name
+        for rid in res_ids_sorted:
+            crm_lead_id = self.env['crm.lead'].browse(rid)
+            if crm_lead_id:
+                res_id_lst.append((rid, res_id_to_name[rid], crm_lead_id.partner_id.name))
+            else:
+                res_id_lst.append((rid, res_id_to_name[rid], False))
+        
+        return {
+            'activity_types': activity_type_infos,
+            'res_ids': res_id_lst,
+            'grouped_activities': activity_data,
+            'model': res_model,
+        }
 
 class MailActivityMixin(models.AbstractModel):
     _inherit = 'mail.activity.mixin'
